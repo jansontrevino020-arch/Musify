0730c54e96205682652b04eff70e50b38c75dbe5
jansontrevino020-arch <jansontrevino020@gmail.com>
2026-04-07 18:55:34 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,11 +15,7 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
-    ).then(() => self.clients.claim())
-  );
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
